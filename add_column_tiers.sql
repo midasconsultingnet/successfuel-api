247f317b307316bb0732cf4cb8324acd0fa0c0d4
Rado Razafindraibe <razafindraiber@gmail.com>
2025-12-12 20:27:53 +0300

    methode de paiement

--- add_column_tiers.sql
+++
@@ -1,8 +0,0 @@
--- Ajouter la colonne manquante donnees_personnelles à la table tiers
-ALTER TABLE tiers ADD COLUMN IF NOT EXISTS donnees_personnelles JSONB;
-
--- Ajouter la colonne manquante station_ids à la table tiers
-ALTER TABLE tiers ADD COLUMN IF NOT EXISTS station_ids JSONB DEFAULT '[]';
-
--- Ajouter la colonne manquante metadonnees à la table tiers
-ALTER TABLE tiers ADD COLUMN IF NOT EXISTS metadonnees JSONB;
